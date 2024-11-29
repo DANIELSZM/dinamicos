06df011c401f849df027e6aecd96663c1f54f809
unknown <danielzeta187@gmail.com>
2024-11-29 12:24:21 -0500

    resolver errores - cambio3

--- db.js
+++ db.js
@@ -9,6 +9,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
             rejectUnauthorized: false, // Solo si Render lo requiere
         },
     },
+    timezone: '-05:00', // Cambia esto a tu zona horaria local (Ejemplo: Colombia es -05:00)
 });
 
 console.log(process.env.DATABASE_URL);
